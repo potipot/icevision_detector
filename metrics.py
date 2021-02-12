263cd0830af7b15b063bf6494c9908acfd19c633
Pawel Potrykus <p.potrykus@toucan-systems.pl>
2021-02-12 19:12:57 +0100

    added DetectedBBox wrapper for non-fork of icevision, added fix for unknown labels

--- metrics.py
+++ metrics.py
@@ -1,11 +1,25 @@
 import itertools
-from typing import Dict, List, Tuple
+from typing import Dict, Sequence
 
 import sklearn
 import torch
 import numpy as np
+from icevision import BBox
 
-from icevision import DetectedBBox
+try:
+    from icevision import DetectedBBox
+except ImportError:
+
+    class DetectedBBox(BBox):
+        def __init__(self, xmin, ymin, xmax, ymax, score, label):
+            if score > 1.0:
+                raise RuntimeWarning(f"setting detection score larger than 1.0: {score}")
+            if score < 0.0:
+                raise RuntimeWarning(f"setting detection score lower than 0.0: {score}")
+            self.score = score
+            self.label = label
+            super(DetectedBBox, self).__init__(xmin, ymin, xmax, ymax)
+
 from pytorch_lightning.metrics import ConfusionMatrix, Metric
 from pytorch_lightning.loggers import wandb as pl_wandb
 from torchvision.ops import box_iou
@@ -18,7 +32,7 @@ def zeroify_items_below_threshold(iou_scores:torch.Tensor, threshold:float) -> t
     return iou_scores*(iou_scores>threshold).byte()
 
 
-def couple_with_targets(predicted_bboxes, iou_scores) -> List:
+def couple_with_targets(predicted_bboxes, iou_scores) -> Sequence:
     """Connects detected bounding boxes with ground truths by iou > 0"""
     ious_per_target = iou_scores.split(1, dim=1)
     return [list(itertools.compress(predicted_bboxes, iou.bool())) for iou in ious_per_target]
@@ -40,13 +54,35 @@ def pick_best_score_labels(predicted_bboxes, confidence_threshold: float = 0.5):
     return best_labels
 
 
-def pairwise_iou(predicted_bboxes, target_bboxes):
+def pairwise_iou(predicted_bboxes: Sequence[BBox], target_bboxes: Sequence[BBox]):
+    """
+    Calculates pairwise iou on lists of bounding boxes. Uses torchvision implementation of `box_iou`.
+    :param predicted_bboxes:
+    :param target_bboxes:
+    :return:
+    """
     stacked_preds = [bbox.to_tensor() for bbox in predicted_bboxes]
     stacked_preds = torch.stack(stacked_preds) if stacked_preds else torch.empty(0, 4)
 
     stacked_targets = [bbox.to_tensor() for bbox in target_bboxes]
     stacked_targets = torch.stack(stacked_targets) if stacked_targets else torch.empty(0, 4)
     return box_iou(stacked_preds, stacked_targets)
+
+
+def add_unknown_labels(ground_truths, predictions, class_map):
+    """
+    Add Missing labels to the class map by turning gt and preds to sets and then checking which idxs are missing
+    :param ground_truths:
+    :param predictions:
+    :param class_map:
+    :return class_map with added missing keys:
+    """
+    ground_truth_idxs = set(ground_truths)
+    prediction_idxs = set(predictions)
+    class_map_idxs = set(class_map._class2id.values())
+    for missing_idx in (ground_truth_idxs.union(prediction_idxs) - class_map_idxs):
+        class_map.add_name(f'unknown_id_{missing_idx}')
+    return class_map
 
 
 class SimpleConfusionMatrix(IceMetric):
@@ -56,16 +92,15 @@ class SimpleConfusionMatrix(IceMetric):
         self.predictions = []
         self._confidence_threshold = confidence_threshold
         self._iou_threshold = iou_threshold
-        self.labels = None
+        self.class_map = None
         self.cm_display = None
 
     def _reset(self):
         self.ground_truths = []
         self.predictions = []
-        self.labels = None
 
     def accumulate(self, records, preds):
-        if self.labels is None: self.labels = next(iter(records))['class_map']._id2class
+        if self.class_map is None: self.class_map = next(iter(records))['class_map']
         for image_targets, image_preds in zip(records, preds):
             target_bboxes = image_targets['bboxes']
             target_labels = image_targets['labels']
@@ -88,15 +123,17 @@ class SimpleConfusionMatrix(IceMetric):
             predicted_labels = pick_best_score_labels(predicted_bboxes, confidence_threshold=self._confidence_threshold)
 
             assert(len(predicted_labels) == len(target_labels))
+            # TODO: find a way to store just the CM instead of entire list of gts/preds
             self.ground_truths.extend(target_labels)
             self.predictions.extend(predicted_labels)
 
     def finalize(self):
         assert(len(self.ground_truths) == len(self.predictions))
+        self.class_map = add_unknown_labels(self.ground_truths, self.predictions, self.class_map)
         self.ground_truths = np.array(self.ground_truths)
         self.predictions = np.array(self.predictions)
         confusion_matrix = sklearn.metrics.confusion_matrix(y_true=self.ground_truths, y_pred=self.predictions)
-        self.cm_display = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix, display_labels=self.labels)
+        self.cm_display = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix, display_labels=self.class_map._id2class)
         return {'dummy_value_for_fastai': -1}
 
     def plot(self, **display_args):
@@ -106,11 +143,12 @@ class SimpleConfusionMatrix(IceMetric):
         return self.cm_display.plot(**display_args).figure_
 
     def log(self, logger_object) -> None:
+        # TODO: maybe better log as wandb image? default matrix is not very readable
         if isinstance(logger_object, pl_wandb.WandbLogger):
             # wandb.sklearn.plot_confusion_matrix(y_true=self.ground_truths, y_pred=self.predictions, labels=self.labels)
             logger_object.experiment.log({
                 "conf_mat": wandb.plot.confusion_matrix(
-                    preds=self.predictions, y_true=self.ground_truths, class_names=self.labels)
+                    preds=self.predictions, y_true=self.ground_truths, class_names=self.class_map._id2class)
             })
         self._reset()
         return
