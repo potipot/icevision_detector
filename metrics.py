ef8432c3d331c1c56a274ad126b5da9944702a3f
Pawel Potrykus <p.potrykus@toucan-systems.pl>
2021-02-12 11:51:41 +0100

    add confusion matrix and support its logging on wandb

--- metrics.py
+++ metrics.py
@@ -1,92 +1,118 @@
 import itertools
+from typing import Dict, List, Tuple
+
+import sklearn
 import torch
+import numpy as np
+
+from icevision import DetectedBBox
 from pytorch_lightning.metrics import ConfusionMatrix, Metric
+from pytorch_lightning.loggers import wandb as pl_wandb
 from torchvision.ops import box_iou
+from icevision.metrics.metric import Metric as IceMetric
+
+import wandb
 
 
-class MyConfusionMatrix(ConfusionMatrix):
-    @staticmethod
-    def _get_highest_score_label(predicted_boxes, iou_scores):
-        iou_filter = (iou_scores > 0.5).squeeze()
-        predictions_per_ground_truth = predicted_boxes[iou_filter]
-        # TODO: nondeterministic output of argmax for equal scores (rare?)
-        if not predictions_per_ground_truth.numel(): return 0  # if predictions are empty return background class
-        prediction_scores = predictions_per_ground_truth.narrow(-1, start=0, length=1)
-        prediction_labels = predictions_per_ground_truth.narrow(-1, start=1, length=1).int()
-        best_score_indice = prediction_scores.argmax()
-        return prediction_labels[best_score_indice].item()
+def zeroify_items_below_threshold(iou_scores:torch.Tensor, threshold:float) -> torch.Tensor:
+    return iou_scores*(iou_scores>threshold).byte()
 
-    def update(self, records, preds):
-        batch_target_labels = []
-        batch_labels = []
-        # matching detections with targets
+
+def couple_with_targets(predicted_bboxes, iou_scores) -> List:
+    """Connects detected bounding boxes with ground truths by iou > 0"""
+    ious_per_target = iou_scores.split(1, dim=1)
+    return [list(itertools.compress(predicted_bboxes, iou.bool())) for iou in ious_per_target]
+
+
+def pick_best_score_labels(predicted_bboxes, confidence_threshold: float = 0.5):
+    # fill with dummy if list of predicted labels is empty
+    BACKGROUND_IDX = 0
+    dummy = DetectedBBox(0, 0, 0, 0, score=1.0, label=BACKGROUND_IDX)
+    best_labels = []
+    # pick the label that fits best given ground truth
+    for ground_truth_predictions in predicted_bboxes:
+        ground_truth_predictions = [
+            prediction if prediction.score > confidence_threshold else dummy
+            for prediction in ground_truth_predictions
+        ]
+        best_prediction = max(ground_truth_predictions, key=lambda x: x.score, default=dummy)
+        best_labels.append(best_prediction.label)
+    return best_labels
+
+
+def pairwise_iou(predicted_bboxes, target_bboxes):
+    stacked_preds = [bbox.to_tensor() for bbox in predicted_bboxes]
+    stacked_preds = torch.stack(stacked_preds) if stacked_preds else torch.empty(0, 4)
+
+    stacked_targets = [bbox.to_tensor() for bbox in target_bboxes]
+    stacked_targets = torch.stack(stacked_targets) if stacked_targets else torch.empty(0, 4)
+    return box_iou(stacked_preds, stacked_targets)
+
+
+class SimpleConfusionMatrix(IceMetric):
+    def __init__(self, confidence_threshold: float = 0.001, iou_threshold: float = 0.5):
+        super(SimpleConfusionMatrix, self).__init__()
+        self.ground_truths = []
+        self.predictions = []
+        self._confidence_threshold = confidence_threshold
+        self._iou_threshold = iou_threshold
+        self.labels = None
+        self.cm_display = None
+
+    def _reset(self):
+        self.ground_truths = []
+        self.predictions = []
+        self.labels = None
+
+    def accumulate(self, records, preds):
+        if self.labels is None: self.labels = next(iter(records))['class_map']._id2class
         for image_targets, image_preds in zip(records, preds):
-            target_boxes = torch.Tensor([bbox.xyxy for bbox in image_targets['bboxes']])
-            predicted_bboxes = torch.stack([
-                torch.Tensor([score, label, *bbox.xyxy])  # bind scores, labels and boxes
-                for score, label, bbox in zip(image_preds['scores'], image_preds['labels'], image_preds['bboxes'])
-            ])
-            iou_scores = box_iou(predicted_bboxes.narrow(-1, 2, 4), target_boxes)
-            ious_per_ground_truth = iou_scores.split(split_size=1, dim=1)
-            labels = [self._get_highest_score_label(predicted_bboxes, iou) for iou in ious_per_ground_truth]
+            target_bboxes = image_targets['bboxes']
+            target_labels = image_targets['labels']
+            # skip if empty ground_truths
+            if not target_bboxes: continue
+            predicted_bboxes = [
+                DetectedBBox(*bbox.xyxy, score=score, label=label)
+                for bbox, score, label in zip(image_preds['bboxes'], image_preds['scores'], image_preds['labels'])
+            ]
+            # get torchvision iou scores (requires conversion to tensors)
+            iou_scores = pairwise_iou(predicted_bboxes, target_bboxes)
+            # TODO: see what happens if that_match is empty
+            that_match = torch.any(iou_scores > self._iou_threshold, dim=1)
+            iou_scores = iou_scores[that_match]
+            iou_scores = zeroify_items_below_threshold(iou_scores, threshold=self._iou_threshold)
 
-            batch_labels.append(labels)
-            batch_target_labels.append(image_targets['labels'])
+            # need to use compress cause list indexing with boolean tensor isn't supported
+            predicted_bboxes = list(itertools.compress(predicted_bboxes, that_match))
+            predicted_bboxes = couple_with_targets(predicted_bboxes, iou_scores)
+            predicted_labels = pick_best_score_labels(predicted_bboxes, confidence_threshold=self._confidence_threshold)
 
-        assert(len(batch_labels) == len(batch_target_labels))
-        preds = torch.tensor(list(itertools.chain.from_iterable(batch_labels))).cuda()
-        target = torch.tensor(list(itertools.chain.from_iterable(batch_target_labels))).cuda()
-        super(MyConfusionMatrix, self).update(preds, target)
+            assert(len(predicted_labels) == len(target_labels))
+            self.ground_truths.extend(target_labels)
+            self.predictions.extend(predicted_labels)
+
+    def finalize(self):
+        assert(len(self.ground_truths) == len(self.predictions))
+        self.ground_truths = np.array(self.ground_truths)
+        self.predictions = np.array(self.predictions)
+        confusion_matrix = sklearn.metrics.confusion_matrix(y_true=self.ground_truths, y_pred=self.predictions)
+        self.cm_display = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix, display_labels=self.labels)
+        return {'dummy_value_for_fastai': -1}
+
+    def plot(self, **display_args):
+        """
+        A handle to plot the matrix in a jupyter notebook, potentially this could also be passed to save_fig
+        """
+        return self.cm_display.plot(**display_args).figure_
+
+    def log(self, logger_object) -> None:
+        if isinstance(logger_object, pl_wandb.WandbLogger):
+            # wandb.sklearn.plot_confusion_matrix(y_true=self.ground_truths, y_pred=self.predictions, labels=self.labels)
+            logger_object.experiment.log({
+                "conf_mat": wandb.plot.confusion_matrix(
+                    preds=self.predictions, y_true=self.ground_truths, class_names=self.labels)
+            })
+        self._reset()
+        return
 
 
-class MyCM(Metric):
-    def __init__(self, dist_sync_on_step=False):
-        super().__init__(dist_sync_on_step=dist_sync_on_step)
-
-        self.add_state("ground_truths", default=[], dist_reduce_fx="sum")
-        self.add_state("predictions", default=[], dist_reduce_fx="sum")
-
-    def update(self, preds: torch.Tensor, target: torch.Tensor):
-        preds, target = self._input_format(preds, target)
-        assert preds.shape == target.shape
-
-        self.correct += torch.sum(preds == target)
-        self.total += target.numel()
-
-    def compute(self):
-        return self.correct.float() / self.total
-
-
-class MyConfusionMatrix1(ConfusionMatrix):
-    @staticmethod
-    def _get_highest_score_label(predicted_boxes, iou_scores):
-        iou_filter = (iou_scores > 0.5).squeeze()
-        predictions_per_ground_truth = predicted_boxes[iou_filter]
-        # TODO: nondeterministic output of argmax for equal scores (rare?)
-        if not predictions_per_ground_truth.numel(): return 0  # if predictions are empty return background class
-        prediction_scores = predictions_per_ground_truth.narrow(-1, start=0, length=1)
-        prediction_labels = predictions_per_ground_truth.narrow(-1, start=1, length=1).int()
-        best_score_indice = prediction_scores.argmax()
-        return prediction_labels[best_score_indice].item()
-
-    def update(self, records, preds):
-        batch_target_labels = []
-        batch_labels = []
-        # matching detections with targets
-        for image_targets, image_preds in zip(records, preds):
-            target_boxes = torch.Tensor([bbox.xyxy for bbox in image_targets['bboxes']])
-            predicted_bboxes = torch.stack([
-                torch.Tensor([score, label, *bbox.xyxy])  # bind scores, labels and boxes
-                for score, label, bbox in zip(image_preds['scores'], image_preds['labels'], image_preds['bboxes'])
-            ])
-            iou_scores = box_iou(predicted_bboxes.narrow(-1, 2, 4), target_boxes)
-            ious_per_ground_truth = iou_scores.split(split_size=1, dim=1)
-            labels = [self._get_highest_score_label(predicted_bboxes, iou) for iou in ious_per_ground_truth]
-
-            batch_labels.append(labels)
-            batch_target_labels.append(image_targets['labels'])
-
-        assert(len(batch_labels) == len(batch_target_labels))
-        preds = torch.tensor(list(itertools.chain.from_iterable(batch_labels))).cuda()
-        target = torch.tensor(list(itertools.chain.from_iterable(batch_target_labels))).cuda()
-        super(MyConfusionMatrix, self).update(preds, target)
--- pl_model.py
+++ pl_model.py
@@ -1,17 +1,15 @@
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
 
 import torch
 from torch import nn
 from timm.optim import create_optimizer
 from timm.scheduler import create_scheduler
 
-from icevision import COCOMetric, ClassMap
+from icevision import COCOMetric, ClassMap, Metric
 from icevision.models import efficientdet
-from .metrics import MyConfusionMatrix
-import wandb
-import pytorch_lightning as pl
+from .metrics import SimpleConfusionMatrix
 
 __all__ = ['BaseModel', 'EffDetModel']
 
@@ -44,20 +42,49 @@ class TimmConfig:
 
 
 class BaseModel(efficientdet.lightning.ModelAdapter):
+    def __init__(self, model: nn.Module, metrics: List[Metric] = None):
+        super(BaseModel, self).__init__(model=model, metrics=metrics)
+        self.n_train_dls, self.n_test_dls, self.n_valid_dls = None, None, None
+
     def configure_optimizers(self):
         optimizer = create_optimizer(self.timm_config, self.model)
         lr_scheduler, num_epochs = create_scheduler(self.timm_config, optimizer)
         return [optimizer], [lr_scheduler]
+
+    def setup(self, stage_name):
+        def _get_num_of_dataloaders(dataloader):
+            n: int
+            dl = getattr(self, dataloader)
+            assert(callable(dl))
+            result = dl()
+            if isinstance(result, list): n = len(self.val_dataloader.dataloader)
+            elif isinstance(result, torch.utils.data.dataloader.DataLoader): n = 1
+            elif result is None: n = 0
+            else: raise RuntimeError
+            return n
+        self.n_train_dls = _get_num_of_dataloaders('train_dataloader')
+        self.n_valid_dls = _get_num_of_dataloaders('val_dataloader')
+        self.n_test_dls = _get_num_of_dataloaders('test_dataloader')
+
+    def freeze_to_head(self, train_class_head=True, train_bbox_head=False):
+        """
+        Freezes the model up to the head part.
+        Parameters control whether to train labels classifier and bbox regressor.
+        """
+        self.freeze()
+        for param in self.model.model.box_net.parameters():
+             param.requires_grad = train_bbox_head
+        for param in self.model.model.class_net.parameters():
+            param.requires_grad = train_class_head
+        self.train()
 
 
 class EffDetModel(BaseModel):
     def __init__(self, num_classes: int, img_size: int, model_name: Optional[str] = "tf_efficientdet_lite0", **timm_args):
         model = efficientdet.model(model_name=model_name, num_classes=num_classes, img_size=img_size, pretrained=True)
         # TODO: change this once pl-mAP is merged: https://github.com/PyTorchLightning/pytorch-lightning/pull/4564
-        metrics = [COCOMetric(print_summary=True)]
+        metrics = [COCOMetric(print_summary=True), SimpleConfusionMatrix()]
         self.timm_config = TimmConfig(**timm_args)
-        self.pl_metrics = [MyConfusionMatrix(num_classes=num_classes+1)]
-        self.__num_val_dataloaders: int = 0
         super().__init__(model=model, metrics=metrics)
 
     def validation_step(self, batch, batch_idx, dataset_idx: int = 0):
@@ -73,27 +100,17 @@ class EffDetModel(BaseModel):
         for metric in self.metrics[dataset_idx]:
             metric.accumulate(records=records, preds=preds)
 
-        # for metric in self.pl_metrics[dataset_idx]:
-        #     metric.update(records=records, preds=preds)
-
-        # loggin losses in step
-        self.log_dict(val_losses, prog_bar=True)
+        # logging losses in step
+        self.log_dict(val_losses)
 
     def validation_epoch_end(self, epoch_output):
         # deprecated?? trainer.evaluation_loop.py @ 210 - for pl factory Metrics in self.evaluation_callback_metrics
         # epoch_output is a list of step_outputs per dataloader shape: [0..n_dls, 0..n_batches]
-        for dataset_idx in range(self.__num_val_dataloaders):
+        for dataset_idx in range(len(self.metrics)):
             self.finalize_metrics(dataset_idx)
 
-        # have to log to wandb directly
-        # class_names = self.get_dataset_class_map()._id2class
-        # import numpy as np
-        # self.logger.experiment.log({"conf_mat": wandb.plot.confusion_matrix(
-        #     preds=np.array([1,2,1,4,5]), y_true=np.array([1,1,1,4,5]), class_names=class_names[:6])})
-        # self.logger.experiment.log_artifact()
-
         # the len of this must be kept as len of dataloaders, otherwise pl ignores idx
-        return epoch_output
+        # return epoch_output
 
     def test_step(self, batch, batch_idx, dataset_idx=0):
         return self.validation_step(batch, batch_idx, dataset_idx)
@@ -102,49 +119,25 @@ class EffDetModel(BaseModel):
         self.validation_epoch_end(*args, **kwargs)
 
     def setup(self, stage_name):
-        if isinstance(self.val_dataloader.dataloader, list):
-            self.__num_val_dataloaders = len(self.val_dataloader.dataloader)
-        else:
-            self.__num_val_dataloaders = 1
+        super(EffDetModel, self).setup(stage_name=stage_name)
 
         # create separate metrics for each dataloader
         self.metrics = [
             [deepcopy(metric) for metric in self.metrics]
-            for _ in range(self.__num_val_dataloaders)
+            for _ in range(self.n_valid_dls if stage_name == 'train' else self.n_test_dls)
         ]
-        self.pl_metrics = nn.ModuleList(
-            [nn.ModuleList([deepcopy(pl_metric) for pl_metric in self.pl_metrics])
-             for _ in range(self.__num_val_dataloaders)]
-        )
-
-        # if hasattr(self.test_dataloader, 'dataloader'):
-        #     # create separate metrics for each dataloader
-        #     # TODO fix for single test_dl
-        #     self.metrics = [[deepcopy(metric) for metric in self.metrics] for dl in self.test_dataloader.dataloader]
+        # self.pl_metrics = nn.ModuleList(
+        #     [nn.ModuleList([deepcopy(pl_metric) for pl_metric in self.pl_metrics])
+        #      for _ in range(self.n_valid_dls)]
+        # )
 
     def finalize_metrics(self, dataset_idx: int = 0) -> None:
-        # self.metric is a list of lists so we flatten it
-        # for metric in itertools.chain(*self.metrics):
         for metric in self.metrics[dataset_idx]:
             metric_logs = metric.finalize()
-            for k, v in metric_logs.items():
-                # TODO: metric logging with forced dataset_idx
-                self.log(f"{metric.name}/{k}/dl_idx_{dataset_idx}", v)
-
-    def get_dataset_class_map(self):
-        class_map: ClassMap
-        class_map_valid = self.val_dataloader.dataloader.dataset.records[0].class_map
-        class_map_train = self.train_dataloader.dataloader.dataset.records[0].class_map
-        return class_map_valid
-
-    def freeze_to_head(self, train_class_head=True, train_bbox_head=False):
-        """
-        Freezes the model up to the head part.
-        Parameters control whether to train labels classifier and bbox regressor.
-        """
-        self.freeze()
-        for param in self.model.model.box_net.parameters():
-             param.requires_grad = train_bbox_head
-        for param in self.model.model.class_net.parameters():
-            param.requires_grad = train_class_head
-        self.train()
+            log = getattr(metric, 'log', None)
+            if callable(log):
+                log(self.logger)
+            else:
+                for k, v in metric_logs.items():
+                    # TODO: metric logging with forced dataset_idx
+                    self.log(f"{metric.name}/{k}/dl_idx_{dataset_idx}", v)
